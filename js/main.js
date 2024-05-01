1d14eca22e9898baac35a11b9f30200bef845b11
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com.>
2024-05-01 13:28:14 -0500

    feat: :construction: Implementacion del DOM en la parte izquierda de la pagina, para dar informacion de los cohetes
    se realizo consumo de la API para mostrar informacion general acerca de los lanzamientos de cada cohete, con un ejemplo de 40 lanzamientos

--- js/main.js
+++ js/main.js
@@ -24,3 +24,33 @@ information__2.append(...progressHeightRocket(Totales.height, Rockets3))
 information__2.append(...progressDiameterRocket(Totales.diameter, Rockets3))
 information__2.append(...progressSecondStageDiameterRocket(Totales.composite_diameter, Rockets3))
 information__2.append(...progressSecondStageHeightRocket(Totales.composite_height, Rockets3))
+
+
+////////////////////////////PARTE IZQUIERDA DE LA PAGINA PARA OBTENER DATOS DE LANZAMIENTOS //////////////////////////////////7
+
+import { getAllLaunches } from "./modules/launches.js"; 
+let informationLeft = document.querySelector("#information__1");
+
+(async () => {
+    try {
+        let [rocketData, launchData] = await Promise.all([getAllRockets(), getAllLaunches()]);
+        let launchesToShow = launchData.slice(0, 40);
+        let html = "";
+        launchesToShow.forEach(launch => {
+            html += `
+                <div class="description__container">
+                    <div>
+                        <img src="${launch.links.patch.small}" alt="${launch.name}">
+                    </div>
+                    <div>
+                        <h3>${launch.name}</h3>
+                        <small>${launch.details}</small>
+                    </div>
+                </div>
+            `;
+        });
+        informationLeft.innerHTML = html;
+    } catch (error) {
+        console.error("Error:", error);
+    }
+})();
