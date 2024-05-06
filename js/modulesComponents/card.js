0e0e4ed15d2f0cb0e13223576eca9fb5b7e250a3
CamiloEstebanConchaTorres <156373519+CamiloEstebanConchaTorres@users.noreply.github.com>
2024-05-06 12:41:01 -0500

    feat: :construction: Implementacion de diseño y footer en la pagina de SpaceX para contrastar mas en las barras de progreso y los menus

--- js/modulesComponents/card.js
+++ js/modulesComponents/card.js
@@ -1,17 +1,33 @@
-export const imageRockets = async(flickr_images)=>{
-    let section__image = document.querySelector("#section__image")
+export const imageRockets = async (flickr_images) => {
+    let section__image = document.querySelector("#section__image");
     let divs = [];
+
     flickr_images.forEach(val => {
         let div = document.createElement("div");
-        div.classList.add("carousel__item")
+        div.classList.add("carousel__item");
+
         let img = document.createElement("img");
-        img.setAttribute("src", val)
-        img.setAttribute("referrerpolicy", "no-referrer")
-        div.append(img)
+        img.setAttribute("src", val);
+        img.setAttribute("referrerpolicy", "no-referrer");
+        div.appendChild(img);
+
         divs.push(div);
     });
-    section__image.append(...divs)
-    // <div class="carousel__item">
-    //     <img src="https://farm5.staticflickr.com/4599/38583829295_581f34dd84_b.jpg" referrerpolicy="no-referrer">
-    // </div>
-}
+
+    section__image.append(...divs);
+    let textContainer = document.createElement("div");
+    textContainer.classList.add("text-container");
+    
+    let text1 = document.createElement("div");
+    text1.textContent = "CABIN MICS:";
+    text1.style.color = "white";
+    let text2 = document.createElement("div");
+    text2.textContent = "RECORDING";
+    text2.style.color = "red";
+    textContainer.appendChild(text1);
+    textContainer.appendChild(text2);
+
+    section__image.appendChild(textContainer);
+};
+
+
--- js/modulesComponents/title.js
+++ js/modulesComponents/title.js
@@ -1,5 +1,7 @@
-export const nameRockets = async(name)=>{
+export const nameRockets = async (name) => {
     let header__title = document.querySelector("#header__title");
     header__title.innerHTML = "";
     header__title.textContent = name;
-}
+    header__title.style.fontFamily = "Brush Script MT";
+    header__title.style.color = "#82cfff";
+};
